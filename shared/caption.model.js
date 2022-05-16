fcd8e186717afddf3f853ff88809716569f1c3ff
Gichelle Amon <amongichelle@gmail.com>
2022-05-16 15:07:44 -0700

    ref(model): redeclare properties

--- shared/caption.model.js
+++ shared/caption.model.js
@@ -1,4 +1,8 @@
 export class Caption{
+    topText;
+    bottomText;
+    _id;
+    
     constructor(_id, topText, bottomText){
         this.topText = topText;
         this.bottomText = bottomText;
