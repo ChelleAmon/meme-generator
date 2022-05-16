18d3cfac761c5c92d5525066fe0cac2cca2a0090
Gichelle Amon <amongichelle@gmail.com>
2022-05-16 14:57:52 -0700

    ref(model): remove properties

--- shared/caption.model.js
+++ shared/caption.model.js
@@ -1,8 +1,4 @@
 export class Caption{
-    topText = string;
-    bottomText = string;
-    _id = string;
-
     constructor(_id, topText, bottomText){
         this.topText = topText;
         this.bottomText = bottomText;
