400f7175dd9d8691f6a6c858763e939052286eee
Aman <amandeepk221@gmail.com>
2022-05-23 09:50:02 -0700

    feat(server): create get request

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from 'cors';
+import { CaptionModel } from "./server/caption.schema.js";
 
 const port = 3000;
 const app = express();
@@ -13,6 +14,19 @@ mongoose.connect(mongoUri).then(() => {
     console.log("connected to DB successfully");
 }).catch((err) => {
     console.log("Failed to connect to DB", err);
+});
+
+// create new caption and save it to database
+app.post('/create-caption', function(req, res) {
+    const {topText, bottomText} = req.body;
+    const new_caption = new CaptionModel({
+        topText,
+        bottomText
+    });
+    new_caption
+    .save()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json(err))
 });
 
 app.get('/', function(req,res) {
