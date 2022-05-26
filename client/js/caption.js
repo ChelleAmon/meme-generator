060f2fca15367658363685b2d9e92dec57731e9e
Jacqueline M <jacqueline.martinez6114@gmail.com>
2022-05-26 16:51:53 -0700

    feat(client/canpions.js) re-copied Gichelle's javascript code to learn how the captions display works in a table display

--- client/js/caption.js
+++ client/js/caption.js
@@ -1,9 +1,41 @@
 import { apiService as api} from "../api/api.service.js";
 
 api.get('captions').then(captions => {
+    captions.forEach(caption => {
+        const table = document. querySelector('#caption-table');
+        const rowCaption = document.createElement('tr')
+        const cellTopText = document.createElement('td')
+        const cellBottomText = document.createElement('td')
+        const cellButtons = document.createElement('td')
     
+        const editButton = document.createElement('button')
+        const deleteButton = document.createElement('button')
+    
+    
+        editButton.innerHTML = "Edit"
+        deleteButton.innerHTML = "Delete"
+    
+    
+        cellTopText.innerHTML =  `${caption.topText}`
+        cellBottomText.innerHTML = `${caption.bottomText}`
+    
+    
+        cellButtons.appendChild(editButton)
+        cellButtons.appendChild(deleteButton)
+    
+    
+        rowCaption.appendChild(cellTopText)
+        rowCaption.appendChild(cellBottomText)
+        rowCaption.appendChild(cellButtons)
+    
+        table.appendChild(rowCaption)
+    
+          
+    
+    })
 
 })
+
 
 
 
