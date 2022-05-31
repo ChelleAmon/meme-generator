ae3cae79e859f6331d98fee466e7cd5f5ea8de2b
Gichelle Amon <amongichelle@gmail.com>
2022-05-30 19:40:58 -0700

    feat(fetchAPI): create a delete method

--- client/api/api.service.js
+++ client/api/api.service.js
@@ -2,5 +2,13 @@ export const apiService = ({
     baseUrl: 'http://localhost:3000/',
     get: function(url) {
         return fetch(this.baseUrl + url).then(res => res.json());
+    },
+
+    delete: function(url){
+        return fetch(this.baseUrl + url, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(() => window.location.reload())
     }
 });
