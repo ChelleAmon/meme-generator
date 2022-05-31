a6a63f42b38c71509b7477a2df681e46e42ebab8
Dustin Daves <biginitiald@gmail.com>
2022-05-31 15:10:36 -0700

    pulled current changes into working branch

--- client/js/save-caption.js
+++ client/js/save-caption.js
@@ -1,6 +1,6 @@
 import { apiService as api } from "../api/api.service";
 
-export const saveCaption = (tText,bText) => {
+export const saveCaption = (id,tText,bText) => {
     api.save(`save-caption/${id}`).then(()=> {
         console.log('caption saved')
     }).catch(() => console.error('caption not saved, please try again'))
