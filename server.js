0e8fbe4d979e614ff726b743f7305137aa213dfa
Gichelle Amon <amongichelle@gmail.com>
2022-05-31 21:02:39 -0700

    style(api): clear out some functions

--- server.js
+++ server.js
@@ -1,61 +1,11 @@
-import * as rootConfig from "./server/configs/root.config.js"
-import * as dbConfig from './server/configs/db.config.js'
-import { CaptionModel } from "./server/schemas/caption.schema.js";
+import * as rootConfig from "./server/configs/root.config.js";
+import * as dbConfig from './server/configs/db.config.js';
+import { apiRouter } from './server/routes/api.routes.js';
 
 const port = 3000;
 const app = rootConfig.app;
 
-
-// get all captions from database
-app.get('/captions', function(req, res) {
-    CaptionModel
-    .find()
-    .then(data => {
-        res.json(data)
-    })
-    .catch(err => res.status(500).json(err));
-});
-
-// create new caption and save it to database
-app.post('/create-caption', function(req, res) {
-    const {topText, bottomText} = req.body;
-    const new_caption = new CaptionModel({
-        topText,
-        bottomText
-    });
-    new_caption
-    .save()
-    .then(data => res.json(data))
-    .catch(err => res.status(500).json(err))
-});
-
-
-app.put('/edit-caption/:id', (req,res) => {
-    const _id = req.params.id
-    const { topText, bottomText } = req.body;
-
-    CaptionModel.findOneAndUpdate(
-        {_id: _id}, 
-        {$set: {topText: topText, bottomText: bottomText}},
-        {new: true}
-    )
-    .then (data => res.json(data))
-    .catch(err => res.status(500).json(err))
-})
-
-//delete caption from database by finding the caption's _id and pass to url as a parameter
-app.delete('/delete-caption/:id', (req,res) => {
-    const id = req.params.id;
-
-    CaptionModel.findOneAndRemove({_id: id})
-    .then(data => res.json(data))
-    .catch(err => res.status(500).json(err))
-
-})
-
-app.get('/', function(req,res) {
-    res.json({message: 'test'})
-});
+app.use('/', apiRouter)
 
 app.listen(port, () => {
   console.log(`listening to http://localhost:${port}`);
