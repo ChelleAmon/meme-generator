5bfd1a07ebc429b6a31f97c178004b00ed29765f
Gichelle Amon <amongichelle@gmail.com>
2022-05-31 21:35:31 -0700

    feat(static): add express static method with clientPath

--- server/configs/root.config.js
+++ server/configs/root.config.js
@@ -1,21 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
-
-const app = express();
-app.use(cors())
-app.use(express.json());
-
 
 const envConfig = {
     mongoUri: process.env.mongo_uri,
     localMongoUri: "mongodb://localhost:27017/memeGenerator",
 }
 
+const __dirname = path.resolve();
+const clientPath = path.join(__dirname, '/client')
 
-export { app, envConfig};
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.static(clientPath))
+
+
+export { app, envConfig };
 
 
 
