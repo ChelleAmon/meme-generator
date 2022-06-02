662d20f413f6f0eea4aadcee4b2cae2470a7ca0c
Gichelle Amon <amongichelle@gmail.com>
2022-06-02 10:02:19 -0700

    ref(import): import ranodmize Caption

--- client/meme/index.js
+++ client/meme/index.js
@@ -1,6 +1,9 @@
 import makeAMeme from '../meme/services/create-meme.js';
 import displayModal from '../meme/services/display-info.js';
-
+import * as randomizeCaption from '../caption/services/randomize-caption.js'
 
 makeAMeme();
 displayModal();
+
+
+
